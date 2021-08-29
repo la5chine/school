d2895912d6a27aefce570de2080c85da2370e744
la5chine <khachinechaker@gmail.com>
2021-08-29 15:08:01 +0200

    add paginations

--- school/settings.py
+++ school/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'school.wsgi.application'
 
+#Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
